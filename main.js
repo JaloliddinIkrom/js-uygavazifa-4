9a0dfe77c83f19fefe8f2bd56555ad30e933d8fe
JaloliddinIkrom <jaloliddinikromovvv@gmail.com>
2022-10-08 04:49:49 +0300

    Add heading

--- main.js
+++ main.js
@@ -86,47 +86,155 @@ var elInp1 = document.querySelector('.js-inp1');
 var elInp2 = document.querySelector('.js-inp2'); 
 var elInp3 = document.querySelector('.js-inp3'); 
 var elBtn = document.querySelector('.js-btn');
- 
 
+
+ let nameNew = []; 
+ let users = [];
+
+
+   
+ let Contact = (array, node) => { 
+
+  node.innerHTML = '';
  
+  array.forEach((item) => { 
+
+
+    var elItem = document.createElement('li'); 
+    elItem.setAttribute('class', 'bg-dark text-white p-3 rounded mb-2')
+      
+      var elTitle = document.createElement("h2"); 
+      elTitle.textContent = item.name;  
+      elItem.appendChild(elTitle);
+     
+      
+      var elLink = document.createElement("a");  
+       elLink.textContent = item.email; 
+       elLink.href = `email:${item.email}`; 
+       elLink.style.display = "block"; 
+       elLink.style.color = "red"; 
+       elLink.setAttribute('class', 'text-decoration-none')
+       elItem.appendChild(elLink); 
+     
+  
+      var elTel = document.createElement("a");
+      elTel.textContent = item.tel; 
+      elTel.href = `tel:${item.tel}`;  
+      elTel.style.display = "block"; 
+      elTel.style.color = "blue"; 
+      elItem.appendChild(elTel);  
+      elTel.setAttribute('class', 'text-decoration-none')
+      
+  
+      var elButten = document.createElement('button');
+      elButten.textContent = 'DELETE';
+      elButten.type = 'submit';
+      elButten.setAttribute('class', 'btn btn-danger ms-3 js-btn'); 
+      elButten.dataset.daletId = item.id;
+       elItem.appendChild(elButten);
+       elList.appendChild(elItem); 
+  });  
+ }   
+
+
+
+ ///=================================================================================================
 
 elForm.addEventListener('submit', function (evt){ 
-
+     
     evt.preventDefault();  
-
+  
+    elList.innerHTML = '';
 
     var elVal1 = elInp1.value; 
     var elVal2 = elInp2.value; 
     var elVal3 = elInp3.value;
 
-  var elItem = document.createElement('li');
-    
-    var elTitle = document.createElement("h2"); 
-    elTitle.textContent = elVal1;  
-    elItem.appendChild(elTitle);
-    elInp1.value = "";
-    
-    var elLink = document.createElement("a");  
-     elLink.textContent = elVal2; 
+  
+    let neIndex = users.findIndex((item) => item.tel == elVal3);
 
-     elLink.href = `email:${elVal2}`; 
-     elLink.style.display = "block"; 
-     elLink.style.color = "red";
-     elItem.appendChild(elLink); 
-     elInp2.value = "";
+    console.log(neIndex);
 
-    var elTel = document.createElement("a");
-    elTel.textContent = elVal3; 
-    elTel.href = `tel:${elVal3}`;  
-    elTel.style.display = "block"; 
-    elTel.style.color = "blue"; 
-    elItem.appendChild(elTel); 
-    elInp3.value = "";
+     if(neIndex >= 0){ 
+
+      alert("Bu raqam oldin kiritilgan boshqa raqam kiriting.."); 
+  
+     }else{ 
+        
+      elInp1.value = '';
+      elInp2.value = ''; 
+      elInp3.value = ''; 
+
+      users.push({ 
+
+         id: users.length + 1,
+         name:  elVal1,
+         email: elVal2,
+         tel: elVal3,
+
+      });
+     
+     } 
+   
+    Contact(users, elList); 
+    nameNew.push(elVal3);   
+
+
+
+}); 
  
-   elList.appendChild(elItem);  
-    
-});
-    
+  
+elList.addEventListener('click', function(evt){ 
+
+if(evt.target.matches('.js-btn')){ 
+
+var userT = evt.target.dataset.daletId; 
+
+var dateUsers = users.findIndex((num) => num.id == userT);
+ 
+users.splice(dateUsers, 1); 
+Contact(users, elList);  
+
+
+}
+
+});   
+
+
+//======================================================================================================
+
+let elButt = document.querySelector('.js-butten');
+
+  var TurFol = false;
+
+  elButt.addEventListener('click', function(){ 
+ 
+    TurFol = !TurFol; 
+     
+    var netur = TurFol ? 'dark' : 'light';
+     
+    window.localStorage.setItem('TurFol', netur); 
+
+   
+   domgachiqaradi();
+  });  
+
+  function domgachiqaradi (){ 
+
+    if(window.localStorage.getItem('TurFol') == 'dark'){ 
+
+      document.body.classList.add('dark'); 
+
+    }else{ 
+
+      document.body.classList.remove('dark'); 
+   
+    }
+  } 
+  domgachiqaradi();
+
+   
+
 
 
 
